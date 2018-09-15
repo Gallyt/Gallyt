4cbea223ee93d596522a3247993d36812c850598
Duarte David <deltaduartedavid@gmail.com>
2018-09-15 15:57:37 +0200

    Unify coding style

--- src/components/add-repo-page/index.tsx
+++ src/components/add-repo-page/index.tsx
@@ -1,48 +1,7 @@
 import { FormNextLink } from 'grommet-icons';
 import * as React from 'react';
 import { RouteComponentProps, withRouter } from 'react-router';
-import styled from 'styled-components';
-
-const color = (name: string) => ({ theme: { colors } }: { theme: { colors: any } }) => colors[name];
-
-const Centered = styled.div`
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  display: flex;
-`;
-
-const Background = styled.div`
-  background-color: ${color('primary')};
-  width: 100vw;
-  height: 100vh;
-`;
-
-const Input = styled.input`
-  width: 30vw;
-  border: none;
-  display: block;
-  background: white;
-  margin: 2px;
-`;
-
-const Button = styled.button`
-  display: block;
-  margin: 2px;
-  border: solid 1px white;
-  color: white;
-  fill: white;
-  stroke: white;
-  background-color: inherit;
-
-  :hover {
-    border-color: ${color('secondary')};
-    color: ${color('secondary')};
-    fill: ${color('secondary')};
-    stroke: ${color('secondary')};
-  }
-`;
+import { Background, Button, Centered, Input } from './style';
 
 class AddRepo extends React.PureComponent<RouteComponentProps, { value: string }> {
   public state = {
---
+++ src/helpers/styled.ts
@@ -0,0 +1,3 @@
+import { Theme } from '../theme';
+
+export const color = (name: keyof Theme['colors']) => ({ theme }: { theme?: Theme }) => theme!.colors[name];
