9a75315403670068397054547272b1801ba63d74
Esteban Sotillo <westixy@gmail.com>
2018-09-15 14:41:22 +0200

    rm link to tree view

--- src/components/app/index.tsx
+++ src/components/app/index.tsx
@@ -3,7 +3,6 @@ import { renderRoutes } from 'react-router-config';
 import { BrowserRouter } from 'react-router-dom';
 
 import ThemeProvider from '../theme-provider';
-import TreeView from '../tree/treeView';
 
 import normalizeCss from '../../helpers/normalize-css';
 import routes from '../../routes';
@@ -13,34 +12,7 @@ normalizeCss();
 const App: React.SFC = () => (
   <BrowserRouter>
     <ThemeProvider>
-      <>
-        {renderRoutes(routes)}
-        <TreeView
-          root={{
-            files: [
-              'package.json',
-              '.gitignore',
-              {
-                files: [
-                  'app.jsx',
-                  'app.css',
-                  {
-                    files: [],
-                    name: 'Junkrat',
-                  },
-                  'oneMoreFile.ts',
-                  {
-                    files: ['a', 'b', 'c'],
-                    name: 'abc',
-                  },
-                ],
-                name: 'src',
-              },
-            ],
-            name: 'test',
-          }}
-        />
-      </>
+      <>{renderRoutes(routes)}</>
     </ThemeProvider>
   </BrowserRouter>
 );
