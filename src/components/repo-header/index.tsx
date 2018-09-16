8bd7524c05f92847456b10993df75e5fd09d049c
Duarte David <deltaduartedavid@gmail.com>
2018-09-16 23:04:35 +0200

    Open vote in new window

--- src/components/repo-header/index.tsx
+++ src/components/repo-header/index.tsx
@@ -2,21 +2,26 @@ import * as React from 'react';
 
 import { Brand, Header, Link, Links, Logo, Vote } from './style';
 
-const RepoHeader: React.SFC<{ repoUrl: string }> = ({ repoUrl }) => (
-  <Header>
-    <Brand to="/">
-      <Logo src="/logo.png" />
-      Gallyt
-    </Brand>
-    <Links>
-      <Link to={`/repo/${repoUrl}`}>File system</Link>
-      <Link to={`/repo/${repoUrl}/commits`}>Commits</Link>
-      <Link to={`/repo/${repoUrl}/commits`}>Commits</Link>
-      <Vote href="https://www.reactriot.com/entries/188-es-community/vote" target="_blank">
-        Vote for us
-      </Vote>
-    </Links>
-  </Header>
-);
+const VOTE_URL = 'https://www.reactriot.com/entries/188-es-community/vote';
+
+const RepoHeader: React.SFC<{ repoUrl: string }> = ({ repoUrl }) => {
+  const onClick = () => !window.open(VOTE_URL, 'ReactRiot', 'width=500,height=500');
+
+  return (
+    <Header>
+      <Brand to="/">
+        <Logo src="/logo.png" />
+        Gallyt
+      </Brand>
+      <Links>
+        <Link to={`/repo/${repoUrl}`}>File system</Link>
+        <Link to={`/repo/${repoUrl}/commits`}>Commits</Link>
+        <Vote href={VOTE_URL} target="_blank" onClick={onClick}>
+          Vote for us
+        </Vote>
+      </Links>
+    </Header>
+  );
+};
 
 export default RepoHeader;
